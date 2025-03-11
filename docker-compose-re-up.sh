8b8d8e249a613193a313316c58d3238e4e40ac2a
murakami0923 <murakami77@nifty.com>
2025-03-12 00:29:53 +0900

    ディレクトリの権限の変更先が誤っていたので修正

--- docker-compose-re-up.sh
+++ docker-compose-re-up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "ディレクトリの権限を変更します"
-echo "- docker/data"
-chmod 2777 docker/data
-ls -l -a docker/
+echo "- solr/data"
+chmod 2777 solr/data
+ls -l -a solr/
 echo "downします"
 docker compose down
 echo "5秒待ちます"
