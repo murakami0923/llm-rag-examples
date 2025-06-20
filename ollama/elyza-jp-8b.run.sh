87e37c71b83bcee4b5e3e51b3d62c5fbc441a2b7
murakami <murakami@bitstar.jp>
2025-06-20 11:47:28 +0900

    ELYZAのモデルの扱いを廃止

--- ollama/elyza-jp-8b.run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it llm-rag-examples-ollama ollama run elyza:jp8b
--- ollama/work/Llama-3-ELYZA-JP-8B/create-model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /root/work/Llama-3-ELYZA-JP-8B/
-ollama create elyza:jp8b -f Modelfile
-
