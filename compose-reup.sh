66da9b42ed8f0e4853e043744e28e45468ee3d09
murakami0923 <murakami77@nifty.com>
2025-07-02 00:16:05 +0900

    pgvectorの定義を追加
    参考：
    https://takumi-oda.com/blog/2025/04/27/post-4500/

--- compose-reup.sh
+++ compose-reup.sh
@@ -6,6 +6,9 @@ ls -l -a elasticsearch/
 echo "- solr/data"
 chmod 2777 solr/data
 ls -l -a solr/
+echo "- pgvector/data"
+chmod 777 pgvector/data
+ls -l -a pgvector/
 
 echo "downします"
 docker compose down
